28a7c2d1d3197c146daf3fc18fc42254b0f6dca2
toubadarou <toubadarou399@gmail.com>
2025-04-25 10:31:47 +0000

    feat: restructure middlewares to include detailed configuration for security and CORS

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,30 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://al-ahzar-backend.onrender.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://al-ahzar-backend.onrender.com'],
+          'script-src': ["'self'", 'https://al-ahzar-backend.onrender.com'],
+          'script-src-elem': ["'self'", 'https://al-ahzar-backend.onrender.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://al-ahzar-front.vercel.app', 'http://localhost:3000', 'https://al-ahzar-backend.onrender.com'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
